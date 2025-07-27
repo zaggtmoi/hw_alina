b30e5f0c2d2fc2f42e2ba0a7097af32dc660340b
zaggtmoi <a.myskova@yandex.ru>
2025-07-27 19:06:23 +0300

    SimpleHashMap + simple tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        //HashMap<Integer, Integer>
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(null, 5);
+        System.out.println(map.get(null));
     }
 }
