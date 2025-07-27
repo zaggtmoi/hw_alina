5ca0182377738e1a695ee30c5f6da125d26f98aa
zaggtmoi <a.myskova@yandex.ru>
2025-07-27 19:17:15 +0300

    clear

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,7 @@
 package org.example;
 
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(null, 5);
-        System.out.println(map.get(null));
+
     }
 }
