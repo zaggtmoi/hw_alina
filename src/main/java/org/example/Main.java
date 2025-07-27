b03885d958bc54f957eb08f827557850cebd8410
zaggtmoi <a.myskova@yandex.ru>
2025-07-27 16:30:50 +0300

    init-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-
+        //HashMap<Integer, Integer>
     }
 }
